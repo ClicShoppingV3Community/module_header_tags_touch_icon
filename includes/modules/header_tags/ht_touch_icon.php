92c748d7fb61c91db41768f883e9179eeef2ce60
ClicShopping <Yae8mr2o*>
2021-03-06 10:37:55 +0100

    update

--- includes/modules/header_tags/ht_touch_icon.php
+++ includes/modules/header_tags/ht_touch_icon.php
@@ -15,8 +15,8 @@
 
   class ht_touch_icon
   {
-    public $code;
-    public $group;
+    public string $code;
+    public string $group;
     public string $title;
     public string $description;
     public ?int $sort_order = 0;
@@ -30,7 +30,7 @@
       $this->title = CLICSHOPPING::getDef('module_header_tags_touch_icon_title');
       $this->description = CLICSHOPPING::getDef('module_header_tags_touch_icon_description');
 
-      if (defined('MODULE_HEADER_TAGS_TOUCH_ICON_STATUS')) {
+      if (\defined('MODULE_HEADER_TAGS_TOUCH_ICON_STATUS')) {
         $this->sort_order = MODULE_HEADER_TAGS_TOUCH_ICON_SORT_ORDER;
         $this->enabled = (MODULE_HEADER_TAGS_TOUCH_ICON_STATUS == 'True');
       }
@@ -51,7 +51,7 @@
 
     public function check()
     {
-      return defined('MODULE_HEADER_TAGS_TOUCH_ICON_STATUS');
+      return \defined('MODULE_HEADER_TAGS_TOUCH_ICON_STATUS');
     }
 
     public function install()
